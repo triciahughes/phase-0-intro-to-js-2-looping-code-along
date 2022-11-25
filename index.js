1e517880a2d3df26d086bf56b3f55a67431189a9
triciahughes <hughes.a.tricia@gmail.com>
2022-11-25 12:24:13 -0800

    commit tree 6acc29cc1400fe4c14c51041e31ca06a3ea41d84

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, additionalMessage) {
+    let tempMessages = [];
+
+    for ( let i = 0; i < names.length; i++) {
+    tempMessages.push(`Thank you, ${names[i]}, for the wonderful ${additionalMessage} gift!`);
+    }
+    return tempMessages;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i <= number && i >= 0) {
+        console.log(i--);
+    }
+}
+
+
